48fc492b6b327776e0ee1526c645b55624f8185b
yilie.wang <wangyilie@outlook.com>
2023-03-02 18:24:00 +0800

    chore: 增加 git commit message 规范校验，增加 preinstall与postinstall 执行脚本

---
+++ scripts/preinstall.js
@@ -0,0 +1,10 @@
+// @ts-check
+// eslint-disable-next-line no-undef
+if (!/pnpm/.test(process.env.npm_execpath || '')) {
+  console.warn(
+    `\u001b[33mThis repository requires using pnpm as the package manager ` +
+      ` for scripts to work properly.\u001b[39m\n`
+  )
+  // eslint-disable-next-line no-undef
+  process.exit(1)
+}
